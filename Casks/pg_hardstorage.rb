d501e48d432c782e3bafb751d5c76d171c832270
cybertec-postgresql <office@cybertec.at>
2026-06-27 07:35:02 +0200

    pg_hardstorage: update cask to v1.0.6 (manual; goreleaser auto-publish still blocked on tap-token 401)

--- Casks/pg_hardstorage.rb
+++ Casks/pg_hardstorage.rb
@@ -1,20 +1,20 @@
 cask "pg_hardstorage" do
-  version "1.0.4"
+  version "1.0.6"
 
   on_macos do
     on_arm do
       url "https://github.com/cybertec-postgresql/pg_hardstorage/releases/download/v#{version}/pg_hardstorage_#{version}_darwin_arm64.tar.gz"
-      sha256 "94cd210dd13a29e7addcea3f02be1a66bf9e0beb013e5f37da109f5c22921671"
+      sha256 "e52b824ff1482bb07a4a59b2cc1a0898adc4effbee5bc411039ca81209f4d217"
     end
   end
   on_linux do
     on_intel do
       url "https://github.com/cybertec-postgresql/pg_hardstorage/releases/download/v#{version}/pg_hardstorage_#{version}_linux_amd64.tar.gz"
-      sha256 "f02674d8bd7684447487fdc352f53dba16caff3b99ece224a98e54b73287d33f"
+      sha256 "c9a1172ddd49bad9513379ff5f8be839bc38f2e68b0a5d4eb6cb4902d2fe317f"
     end
     on_arm do
       url "https://github.com/cybertec-postgresql/pg_hardstorage/releases/download/v#{version}/pg_hardstorage_#{version}_linux_arm64.tar.gz"
-      sha256 "28ed251b4aa6dd15b122679b0dd622f9bc8ea52bf72dfb573991c66e9354cc4a"
+      sha256 "3b7d41ce91566f15f1a2ee50847e2abe882176bff6129c3c8954f1675534fb5c"
     end
   end
 
