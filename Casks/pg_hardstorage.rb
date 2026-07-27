62f9c72cf1b5fb3d000a3177ee16ab45e2b85e1c
Hans-Jürgen Schönig <hs@cybertec.at>
2026-07-27 11:53:14 +0200

    pg_hardstorage: update cask to 1.0.17

--- Casks/pg_hardstorage.rb
+++ Casks/pg_hardstorage.rb
@@ -1,20 +1,20 @@
 cask "pg_hardstorage" do
-  version "1.0.16"
+  version "1.0.17"
 
   on_macos do
     on_arm do
       url "https://github.com/cybertec-postgresql/pg_hardstorage/releases/download/v#{version}/pg_hardstorage_#{version}_darwin_arm64.tar.gz"
-      sha256 "f71393fbc14c30385fd1f8266b6e191bbc583512f6d362e10fc402f4bdfc2b86"
+      sha256 "325feeddd6439629f287301479d146da963ffc40e6c4dad52c04c39e75456899"
     end
   end
   on_linux do
     on_intel do
       url "https://github.com/cybertec-postgresql/pg_hardstorage/releases/download/v#{version}/pg_hardstorage_#{version}_linux_amd64.tar.gz"
-      sha256 "3b3a9432d134791440dc65e8ab1b870fe8effdfbac927ab627c06dc760d89ba4"
+      sha256 "0b8c7d4fb63aa2a32dea78c6811405555a00b5e79408b05a5078d745fee13a3a"
     end
     on_arm do
       url "https://github.com/cybertec-postgresql/pg_hardstorage/releases/download/v#{version}/pg_hardstorage_#{version}_linux_arm64.tar.gz"
-      sha256 "d0a627fcacf68475298c6b1bac11e61e116534d85bb4a886ec638764d7685b8d"
+      sha256 "ca6bd22d31b886eafd2a38bd99cb644a610159c4f50ca6c96e5751eeaee66ba8"
     end
   end
 
